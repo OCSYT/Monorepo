34add5875411fa17a7c1d07ca42e06f4ec7fa4c0
BracketProto <69521768+OCSYT@users.noreply.github.com>
2025-05-19 16:19:05 +0100

    update CORS

--- Server/App.js
+++ Server/App.js
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from "cors";
 const App = express();
 const PORT = 8080;
+
+const CORSOptions = {
+	origin: "*",
+	methods: ["GET", "POST"],
+	allowedHeaders: ["Content-Type"],
+};
+
 App.use(express.json());
+App.use(cors(CORSOptions));
 
 App.get("/", (req, res) => {
 	res.json({ Message: "API Response Successful!" });
